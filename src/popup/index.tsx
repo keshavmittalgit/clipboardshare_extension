9bed4a11bff7ee40f9f151cd75436317a009a881
keshavmittalgit <keshavmittal7974@gmail.com>
2024-12-13 22:22:51 +0530

    and

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'; // Relative path, correct
 import Popup from './popup';
-import '../tailwind.css';
+import '../assets/tailwind.css';
 
 const container = document.createElement('div');
 document.body.appendChild(container);
